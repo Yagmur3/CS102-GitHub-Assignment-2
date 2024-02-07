bbe72423b6ab1461e319f21172fd8b436d7f0106
Gokay Nuray <146674277+GokayNuray@users.noreply.github.com>
2024-02-07 17:02:05 +0300

    Update Main.java
    Fixed wrong spacing between curvy braces and parantheses

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import java.lang.Math;
 /**
  * This is a menu-driven console application that supports many operations on an array of integers.
  */
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
 
         //Welcoming the user and asking for the size of the array
         System.out.println("Welcome to the application");
@@ -15,7 +15,7 @@ public class Main{
         //Creating the array and filling it with random numbers from 0 to 100
         int size = scanner.nextInt();
         int[] array = new int[size];
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             array[i] = (int) (Math.random() * 101);
         }
 
